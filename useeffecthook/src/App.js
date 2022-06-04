ee64216aa6f2bc348da14650b934bc50700a3356
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-04 12:56:28 +0000

    removed dependency array from useefect

--- useeffecthook/src/App.js
+++ useeffecthook/src/App.js
@@ -5,12 +5,14 @@ const App = () => {
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  }, []);
+  });
 
-  return <div>
-    <p>You clicked {count} times</p>
-    <button onClick={() => setCount(count + 1)}>Click me</button>
-  </div>;
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
 };
 
 export default App;
