039b678dd78d0ceb7c75c09ad570ec7e6c2fbab5
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-04 13:50:35 +0000

    timer cleanup function

--- useeffecthook/src/App.js
+++ useeffecthook/src/App.js
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    // calculate the product of the values of count by 2
-    setCalculation(count * 2);
-  }, [count]); // count becomes a dependency for the calculation to work
+    let timer = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    // cleanup
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((count) => count + 1)}>+</button>
-      <p>Calculation: {calculation}</p>
+      <p>I have rendered {count} times.</p>
     </div>
   );
 };
