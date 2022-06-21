5700d3db10d51c5edd05790f53fcf10774dda4e9
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-21 16:29:51 +0000

    still trying something

--- child-to-parent/src/App.js
+++ child-to-parent/src/App.js
@@ -8,15 +8,21 @@ export default function App() {
     const enteredData = {
       ...data,
     };
-    console.log("from app js");
     myData.push(enteredData);
     console.log(myData);
+    console.log(myData[0].enteredEmail);
   };
 
   return (
     <div className="">
       <h1 className="heading">React Props</h1>
       <h4 className="heading--sec">Child to parent communication</h4>
+      {myData.length !== 0 && myData.map((data, index) => (
+        <div key={index}>
+          <p>{data[index].enteredName}</p>
+          <p>{data[index].enteredEmail}</p>
+        </div>
+      ))}
       <div>
         <DataForm onAddData={addData} />
         <DataPreview />
