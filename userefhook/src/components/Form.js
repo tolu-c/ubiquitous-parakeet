a19980c18b675ec633a3c11c72999a034f0cd9de
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-28 19:11:09 +0000

    managed form data.

--- userefhook/src/components/Form.js
+++ userefhook/src/components/Form.js
@@ -2,18 +2,44 @@ import { useRef } from "react";
 
 const Form = () => {
   const nameRef = useRef();
+  const emailRef = useRef();
 
-  const focusInput = () => {
-    nameRef.current.focus();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+    const enteredName = nameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+
+    const data = {
+      name: enteredName,
+      email: enteredEmail,
+    };
+
+    console.log(data);
+
+    // clears form after submission
+    nameRef.current.value = "";
+    emailRef.current.value = "";
   };
 
   return (
     <div>
       <h2>Form</h2>
 
-      <form action="#">
-        <input type="text" ref={nameRef} />
-        <button onClick={focusInput}>Focus Input</button>
+      <form action="#" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="name">name</label>
+          <input type="text" name="name" id="name" ref={nameRef} />
+        </div>
+
+        <div>
+          <label htmlFor="email">email</label>
+          <input type="email" name="email" id="email" ref={emailRef} />
+        </div>
+
+        <div>
+          <button type="submit">submit</button>
+        </div>
       </form>
     </div>
   );
