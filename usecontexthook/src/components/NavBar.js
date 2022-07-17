9d5b04f8a31d19669ae411988b53c56eda04da6a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-17 19:59:42 +0000

    Added Links to Navbar

--- usecontexthook/src/components/NavBar.js
+++ usecontexthook/src/components/NavBar.js
@@ -1,12 +1,20 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <nav className="flex justify-between">
       <h2>useContext</h2>
 
       <ul className="flex gap-x-8">
-        <li>Home</li>
-        <li>About</li>
-        <li>Login</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="login">Login</Link>
+        </li>
       </ul>
     </nav>
   );
