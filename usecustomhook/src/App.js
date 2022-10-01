e9b111d0a0eb44dd7ff42dd867a1f77a3959dd9f
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-10-01 20:23:13 +0100

    without hooks

--- usecustomhook/src/App.js
+++ usecustomhook/src/App.js
@@ -1,3 +1,48 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>hello world</div>;
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>An error occurred! Pls try again</p>;
+  }
+
+  console.log({ data });
+  return (
+    <div>
+      <h2>React Custom Hook</h2>
+      {data && (
+        <div>
+          <img
+            src={data?.icon_url}
+            alt={data?.value}
+            width={300}
+            height={300}
+          />
+          <p>{data?.value}</p>
+        </div>
+      )}
+    </div>
+  );
 }
