dc74a9d8f9e03d55b1cd46526f456ec513e525ef
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-10-02 17:39:40 +0100

    basic style

--- reactportal/src/App.js
+++ reactportal/src/App.js
@@ -5,13 +5,23 @@ const App = () => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center relative">
       <h2 className="text-3xl font-bold text-gray-900">React Portal</h2>
 
       {openModal ? (
-        <button onClick={() => setOpenModal(false)}>Close Modal</button>
+        <button
+          onClick={() => setOpenModal(false)}
+          className="px-4 py-2 rounded-md bg-blue-600 text-blue-100 font-bold text-lg"
+        >
+          Close Modal
+        </button>
       ) : (
-        <button onClick={() => setOpenModal(true)}>Open Modal</button>
+        <button
+          onClick={() => setOpenModal(true)}
+          className="px-4 py-2 rounded-md bg-blue-600 text-blue-100 font-bold text-lg"
+        >
+          Open Modal
+        </button>
       )}
 
       {openModal && <Modal onClose={setOpenModal} />}
--- reactportal/src/UI/Modal.js
+++ reactportal/src/UI/Modal.js
@@ -4,11 +4,21 @@ const Modal = ({ onClose }) => {
   };
 
   return (
-    <div>
+    <div className="w-5/6 md:w-1/2 border absolute mx-auto p-4 rounded-md shadow-lg bg-white text-slate-800">
       <h2>This is a modal</h2>
-      <div>
-        <button onClick={handleCloseModal}>cancel</button>
-        <button onClick={handleCloseModal}>ok</button>
+      <div className="flex w-full justify-between gap-4 md:gap-6">
+        <button
+          onClick={handleCloseModal}
+          className="rounded-md shadow-sm px-2 py-1 text-center text-base font-medium capitalize flex-grow bg-red-600 text-red-50"
+        >
+          cancel
+        </button>
+        <button
+          onClick={handleCloseModal}
+          className="rounded-md shadow-sm px-2 py-1 text-center text-base font-medium capitalize flex-grow bg-teal-600 text-teal-50"
+        >
+          ok
+        </button>
       </div>
     </div>
   );
