210339b13926d1be2529e5306ce9e34e0df037ca
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-10-02 16:35:41 +0100

    basic modal

--- reactportal/src/App.js
+++ reactportal/src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import Modal from "./UI/Modal";
+
 const App = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div>
       <h2 className="text-3xl font-bold text-gray-900">React Portal</h2>
 
-      <button>Open Modal</button>
+      {openModal ? (
+        <button onClick={() => setOpenModal(false)}>Close Modal</button>
+      ) : (
+        <button onClick={() => setOpenModal(true)}>Open Modal</button>
+      )}
+
+      {openModal && <Modal onClose={setOpenModal} />}
     </div>
   );
 };
--- reactportal/src/UI/Modal.js
+++ reactportal/src/UI/Modal.js
@@ -1,7 +1,17 @@
-import React from "react";
+const Modal = ({ onClose }) => {
+  const handleCloseModal = () => {
+    onClose(false);
+  };
 
-const Modal = () => {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <h2>This is a modal</h2>
+      <div>
+        <button onClick={handleCloseModal}>cancel</button>
+        <button onClick={handleCloseModal}>ok</button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
